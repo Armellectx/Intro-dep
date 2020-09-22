213e97288c0554a7d6eb0fb9096579e7b94e4488
Armellectx <56161210+Armellectx@users.noreply.github.com>
2020-09-22 15:25:19 +0200

    Add files via upload

--- elevator.c
+++ elevator.c
@@ -45,19 +45,40 @@ else { p=liste.*next;}
 }
 
 
+Boolean EstPleine(Elevator*e){// fonction qui dit si l'elevator est plein
+p=e->persons->person;
+int compteur=0;
+while (p!=NULL){
+    compteur=compteur+1;
+    p=p=e->persons->next;
+}if( compteur==e->capacity){return true;}
+else{return false;}
+}
+
+
+//rappel: foo->bar is equivalent to (*foo).bar
+
 PersonList* enterElevator(Elevator *e, PersonList *list){
+
+    //cette fonction fait entrer dans l’ascenseur les personnes qui attendent (waitingList) et renvoie la nouvelle liste d’attente (i.e. la liste initiale moins les personnes qui sont entrées dans l’ascenseur)
+//L’ascenseur ne peut pas accueillir plus de personnes que sa capacité maximale
+
 
     PersonList *attente= (PersonList*)malloc(sizeof(PersonList));//la nouvelle liste d'attente à renvoyer
     liste=b.*elevator.*waitingLists
+    p=*liste.*person; //on commence par regarder la premiere personne sur la liste
+
 
     while (p!=NULL){ //on parcours la liste d'attente 
-        si la personne veut rentrer
-            si la capacité n'est pas pleine
-                on l'ajoue a la liste de la capine
-            sinon: on l'ajoute à la nouvelle liste d'attente 
+        if (EstPleine(*e)==false){
 
-    }
+            e->persons->next=p; //on ajoute la personne à la cabine
+            p=attente->next;
+             }
+              
+            else{attente->next=p;}//sinon: on l'ajoute à la nouvelle liste d'attente 
+            }
+            return *attente;
+        }
 
-}
-void stepElevator(Building *b);
 
